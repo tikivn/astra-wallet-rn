00ce8d15e7a6acc7ff2b1476fe3ad07e12370671
Khanh Vo <khanh.vo@tiki.vn>
2022-07-13 14:03:22 +0700

    Update localization files

--- packages/mobile/src/translations/splits/index.tsx
+++ packages/mobile/src/translations/splits/index.tsx
@@ -1,15 +1,38 @@
+import ComponentEnMsgs from "./component.en.json";
 import ComponentViMsgs from "./component.vi.json";
+import HistoryEnMsgs from "./history.en.json";
 import HistoryViMsgs from "./history.vi.json";
 import StakeEnMsgs from "./stake.en.json";
 import StakeViMsgs from "./stake.vi.json";
+import StakingEnMsgs from "./staking.en.json";
 import StakingViMsgs from "./staking.vi.json";
+import TxEnMsgs from "./tx.en.json";
 import TxViMsgs from "./tx.vi.json";
+import ValidatorEnMsgs from "./validator.en.json";
 import ValidatorViMsgs from "./validator.vi.json";
+import CommonEnMsgs from "./common.en.json";
+import CommonViMsgs from "./common.vi.json";
+import WalletConnectEnMsgs from "./walletconnect.en.json";
+import WalletConnectViMsgs from "./walletconnect.vi.json";
 
 export const ViMsgs = [
-    ComponentViMsgs, HistoryViMsgs, StakeViMsgs, StakingViMsgs, TxViMsgs, ValidatorViMsgs
+  ComponentViMsgs,
+  HistoryViMsgs,
+  StakeViMsgs,
+  StakingViMsgs,
+  TxViMsgs,
+  ValidatorViMsgs,
+  CommonViMsgs,
+  WalletConnectViMsgs,
 ]
 
 export const EnMsgs = [
-    StakeEnMsgs
+  ComponentEnMsgs,
+  HistoryEnMsgs,
+  StakeEnMsgs,
+  StakingEnMsgs,
+  TxEnMsgs,
+  ValidatorEnMsgs,
+  CommonEnMsgs,
+  WalletConnectEnMsgs,
 ]
