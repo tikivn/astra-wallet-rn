fac7bd1b94a8ed73c25baae8abda1b576b2e174e
Duc Nguyen <duc.nguyen@tiki.vn>
2022-09-20 15:53:14 +0700

    Disable tooltip

--- packages/mobile/src/screens/staking/component/tooltip-label.tsx
+++ packages/mobile/src/screens/staking/component/tooltip-label.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { FunctionComponent } from "react";
 import {
-  Image,
+  // Image,
   StyleSheet,
   Text,
   TextStyle,
@@ -9,7 +9,7 @@ import {
   ViewStyle,
 } from "react-native";
 import { useStyle } from "../../../styles";
-import { Popable } from "react-native-popable";
+// import { Popable } from "react-native-popable";
 
 export const TooltipLabel: FunctionComponent<{
   textStyle?: TextStyle;
@@ -18,29 +18,29 @@ export const TooltipLabel: FunctionComponent<{
 }> = observer(({ textStyle, containerStyle, text }) => {
   const style = useStyle();
   return (
-    <Popable content={text}>
-      <View
+    // <Popable content={text}>
+    <View
+      style={StyleSheet.flatten([
+        style.flatten(["flex-row", "justify-start", "items-center"]),
+        containerStyle,
+      ])}
+    >
+      <Text
         style={StyleSheet.flatten([
-          style.flatten(["flex-row", "justify-start", "items-center"]),
-          containerStyle,
+          style.flatten(["color-gray-30", "text-caption2"]),
+          textStyle,
         ])}
       >
-        <Text
-          style={StyleSheet.flatten([
-            style.flatten(["color-gray-30", "text-caption2"]),
-            textStyle,
-          ])}
-        >
-          {text}
-        </Text>
-        <View style={style.flatten(["width-20", "height-16", "items-end"])}>
+        {text}
+      </Text>
+      {/* <View style={style.flatten(["width-20", "height-16", "items-end"])}>
           <Image
             style={style.flatten(["width-16", "height-16"])}
             resizeMode="contain"
             source={require("../../../assets/image/icon_tooltip.png")}
           />
-        </View>
-      </View>
-    </Popable>
+        </View> */}
+    </View>
+    // </Popable>
   );
 });
